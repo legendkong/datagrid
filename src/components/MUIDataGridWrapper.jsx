eb1d2a6aee4ac94ea3729b18671ced52a87b51d1
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-25 17:29:19 +0800

    refactor: return for custom pagination

--- src/components/MUIDataGridWrapper.jsx
+++ src/components/MUIDataGridWrapper.jsx
@@ -25,6 +25,15 @@ function CustomPagination () {
    const apiRef = useGridApiContext()
    const page = useGridSelector(apiRef, gridPageSelector)
    const pageCount = useGridSelector(apiRef, gridPageCountSelector)
+
+   return (
+      <Pagination
+         color='primary'
+         count={pageCount}
+         page={page + 1}
+         onChange={(event, value) => apiRef.current.setPage(value - 1)}
+      />
+   )
 }
 
 function MUIDataGridWrapper() {
