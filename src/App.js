e1aa05c7bb4a0e4aa434c27946e490081e5a0787
Jonathan Kong <jonathankong96@hotmail.com>
2022-06-26 23:07:44 +0800

    feat: trying wrapper with gridOne component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import MuiDataGrid from "./components/MuiDataGrid";
+import gridOne from "./components/gridOne";
 
 function App() {
   return (
     <div className="App">
       <MuiDataGrid />
+      <gridOne />
     </div>
   );
 }
